a2a785df8500a0d4626f54d67f1fa290d985eb09
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2021-04-06 22:43:04 -0600

    Front Page for Web Application

--- src/main/java/com/jitterted/ebp/blackjack/Blackjack.java
+++ src/main/java/com/jitterted/ebp/blackjack/Blackjack.java
@@ -4,7 +4,6 @@ import com.jitterted.ebp.blackjack.adapter.in.console.ConsoleGame;
 import com.jitterted.ebp.blackjack.domain.Game;
 
 public class Blackjack {
-
     public static void main(String[] args) {
         Game game = new Game(); //Model
         ConsoleGame consoleGame = new ConsoleGame(game);
